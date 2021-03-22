8d9d952ed8e6eea6442727821563c237cd714b27
Dione <dr-ramos-slmb@hotmail.com>
2021-03-22 01:26:16 -0300

    Initial commit.
    Signed-off-by: dr-ra <dr-ra@DESKTOP-57765UG>

---
+++ curso-api/src/test/java/com/conquer/cursoapi/CursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conquer.cursoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
